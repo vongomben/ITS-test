88fc44247e0850beeec87a1141cbd00dc5b681c5
vongomben <davide.gomba@gmail.com>
2019-01-23 16:41:00 +0100

    editing code

--- testMillis/testMillis.ino
+++ testMillis/testMillis.ino
@@ -1,23 +1,4 @@
-/*
-  Analog input, analog output, serial output
 
- Reads an analog input pin, maps the result to a range from 0 to 255
- and uses the result to set the pulsewidth modulation (PWM) of an output pin.
- Also prints the results to the serial monitor.
-
- The circuit:
- * potentiometer connected to analog pin 0.
-   Center pin of the potentiometer goes to the analog pin.
-   side pins of the potentiometer go to +5V and ground
- * LED connected from digital pin 9 to ground
-
- created 29 Dec. 2008
- modified 9 Apr 2012
- by Tom Igoe
-
- This example code is in the public domain.
-
- */
 
 // These constants won't change.  They're used to give names
 // to the pins used:
